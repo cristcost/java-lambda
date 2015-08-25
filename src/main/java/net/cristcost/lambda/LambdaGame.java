762b8d8b7f20b871853746aef6f8ee833d423e50
cristcost <cristiano.costantini@gmail.com>
2015-08-25 14:29:35 +0200

    import of the code

---
+++ src/main/java/net/cristcost/lambda/LambdaGame.java
@@ -0,0 +1,202 @@
+/*
+ * Copyright 2015, Cristiano Costantini
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package net.cristcost.lambda;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+@SuppressWarnings("boxing")
+public class LambdaGame {
+
+  // this class runs at least with Java 8
+  public static void main(String[] args) {
+    howManyConsonants();
+    howManyConsonantsWithoutTypeInference();
+    howManyConsonantsWithName();
+    eachStepOfHowManyConsonants();
+    howManyConsonantsWithMethodReferences();
+    howManyConsonantsPseudoJava7();
+    howManyConsonantsPseudoJava7Inlineless();
+    howManyConsonantsJava7();
+  }
+
+  // consider a list of strings
+  static List<String> list =
+      Arrays.asList("Cristiano", "Michele", "Sergio", "Giuseppe", "Stefano");
+
+  // lets count with lambda how many consonants in all the names
+  public static void howManyConsonants() {
+    
+    Integer out = list.stream() // process with a stream
+        .map((p) -> p.replaceAll("[aeiou]", "")) // strip all (lower case) vowels from each name  
+        .map((a) -> a.length()) // count the length of the remaining word
+        .reduce((a, b) -> a + b) // sum each of the previously computed lengths
+        .get(); // get the result
+
+    System.out.println("howManyConsonants: there are " + out + " consonants");
+  }
+
+  // that without type inference looks like
+  public static void howManyConsonantsWithoutTypeInference() {
+    Integer out = list.stream()
+        .map((String p) -> p.replaceAll("[aeiou]", ""))
+        .map((String a) -> a.length())
+        .reduce((Integer a, Integer b) -> a + b)
+        .get();
+
+    System.out.println("howManyConsonantsWithoutTypeInference: there are " + out + " consonants");
+  }
+
+  // we have used the following @FunctionalInterfaces
+  static BinaryOperator<Integer> accumulator = (a, b) -> a + b;
+  static Function<String, Integer> lengthMapper = (a) -> a.length();
+  static Function<String, String> devowelizerMapper = (p) -> p.replaceAll("[aeiou]", "");
+
+  // and we can rewrite it using these above
+  public static void howManyConsonantsWithName() {
+    Integer out = list.stream()
+        .map(devowelizerMapper)
+        .map(lengthMapper)
+        .reduce(accumulator)
+        .get();
+
+    System.out.println("howManyConsonantsWithName: there are " + out + " consonants");
+  }
+
+  // we have used "streams" to process the output, these are the individual
+  // steps
+  public static void eachStepOfHowManyConsonants() {
+
+    Stream<String> stream = list.stream();
+    Stream<String> devowelizedStream = stream.map(devowelizerMapper);
+    Stream<Integer> lengthStream = devowelizedStream.map(lengthMapper);
+    Optional<Integer> reduce = lengthStream.reduce(accumulator);
+    Integer out = reduce.get();
+
+    System.out.println("eachStepOfHowManyConsonants: there are " + out + " consonants");
+  }
+
+  // the lambda function we have used are equivalent to the methods in this
+  // class
+  public static class LambdaMethods {
+
+    public static Integer accumulator(Integer a, Integer b) {
+      return a + b;
+    };
+
+    public static Integer lengthMapper(String s) {
+      return s.length();
+    };
+
+    public static String devowelizerMapper(String s) {
+      return s.replaceAll("[aeiou]", "");
+    };
+  }
+
+  // they are so equivalent that we can implement the methods using them
+  public static class MethodsUsingLambdasImpl {
+
+    public static Integer accumulator(Integer t, Integer u) {
+      return accumulator.apply(t, u);
+    };
+
+    public static Integer lengthMapper(String s) {
+      return lengthMapper.apply(s);
+    };
+
+    public static String devowelizerMapper(String s) {
+      return devowelizerMapper.apply(s);
+    };
+  }
+
+  // methods of a class can be now referenced and used in place of a lambda
+  public static void howManyConsonantsWithMethodReferences() {
+    Integer out = list.stream()
+        .map(LambdaMethods::devowelizerMapper)
+        .map(LambdaMethods::lengthMapper)
+        .reduce(LambdaMethods::accumulator)
+        .get();
+
+    System.out.println("howManyConsonantsWithMethodReferences: there are " + out + " consonants");
+  }
+
+  // if we don't have java 8? we would have used anonymous classes
+  public static void howManyConsonantsPseudoJava7() {
+    Integer out =
+        list.stream().map(new Function<String, String>() {
+
+          @Override
+          public String apply(String p) {
+            return p.replaceAll("[aeiou]", "");
+          }
+        }).map(new Function<String, Integer>() {
+
+          @Override
+          public Integer apply(String a) {
+            return a.length();
+          }
+        }).reduce(new BinaryOperator<Integer>() {
+
+          @Override
+          public Integer apply(Integer a, Integer b) {
+            return a + b;
+          }
+        }).get();
+
+    System.out.println("howManyConsonantsPseudoJava7: there are " + out + " consonants");
+  }
+
+  // let's rewrite it without inlining anonymous classes
+  static Function<String, String> anonymousDevowelizerMapperClass = new Function<String, String>() {
+
+    @Override
+    public String apply(String p) {
+      return p.replaceAll("[aeiou]", "");
+    }
+  };
+  static Function<String, Integer> anonymousLengthMapperClass = new Function<String, Integer>() {
+
+    @Override
+    public Integer apply(String a) {
+      return a.length();
+    }
+  };
+  static BinaryOperator<Integer> anonymousAccumulatorClass = new BinaryOperator<Integer>() {
+
+    @Override
+    public Integer apply(Integer a, Integer b) {
+      return a + b;
+    }
+  };
+
+  // and then re-implement without the inline anonymous classes
+  public static void howManyConsonantsPseudoJava7Inlineless() {
+    Integer out = list.stream()
+        .map(anonymousDevowelizerMapperClass)
+        .map(anonymousLengthMapperClass)
+        .reduce(anonymousAccumulatorClass)
+        .get();
+
+    System.out.println("howManyConsonantsPseudoJava7Inlineless: there are " + out + " consonants");
+  }
+
+  // we haven't used lambda but we are still using Java 8 features, what's without?
+  public static void howManyConsonantsJava7() {
+    LambdaGameWithoutLambda.howManyConsonantsJava7();
+  }
+}
---
+++ src/main/java/net/cristcost/lambda/LambdaGameWithoutLambda.java
@@ -0,0 +1,219 @@
+/*
+ * Copyright 2015, Cristiano Costantini
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package net.cristcost.lambda;
+
+import net.cristcost.lambda.LambdaGameWithoutLambda.SmallJava8.BinaryOperator;
+import net.cristcost.lambda.LambdaGameWithoutLambda.SmallJava8.Function;
+import net.cristcost.lambda.LambdaGameWithoutLambda.SmallJava8.Optional;
+import net.cristcost.lambda.LambdaGameWithoutLambda.SmallJava8.Stream;
+
+import static net.cristcost.lambda.LambdaGameWithoutLambda.SmallJava8.streamFromList;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SuppressWarnings("boxing")
+public class LambdaGameWithoutLambda {
+
+  // this class runs also on Java 7
+  public static void main(String[] args) {
+    howManyConsonantsJava7();
+    eachStepOfHowManyConsonantsJava7();
+  }
+
+  // consider the same list a list of strings
+  static List<String> list =
+      Arrays.asList("Cristiano", "Michele", "Sergio", "Giuseppe", "Stefano");
+
+  // implementation without lambda and without java 8 streams
+  public static void howManyConsonantsJava7() {
+
+    Integer out = streamFromList(list)
+        .map(devowelizerMapperClass)
+        .map(lengthMapperClass)
+        .reduce(accumulatorClass)
+        .get();
+
+    System.out.println("howManyConsonantsJava7: there are " + out + " consonants");
+  }
+
+  // and same implementation in each individual step
+  public static void eachStepOfHowManyConsonantsJava7() {
+
+    Stream<String> stream = streamFromList(list);
+    Stream<String> devowelizedStream = stream.map(devowelizerMapperClass);
+    Stream<Integer> lengthStream = devowelizedStream.map(lengthMapperClass);
+    Optional<Integer> reduce = lengthStream.reduce(accumulatorClass);
+    Integer out = reduce.get();
+
+    System.out.println("eachStepOfHowManyConsonantsJava7: there are " + out + " consonants");
+  }
+
+  // the functions used are inline anonymous classes
+  static Function<String, String> devowelizerMapperClass = new Function<String, String>() {
+
+    @Override
+    public String apply(String p) {
+      return p.replaceAll("[aeiou]", "");
+    }
+  };
+  static Function<String, Integer> lengthMapperClass = new Function<String, Integer>() {
+
+    @Override
+    public Integer apply(String a) {
+      return a.length();
+    }
+  };
+  static BinaryOperator<Integer> accumulatorClass = new BinaryOperator<Integer>() {
+
+    @Override
+    public Integer apply(Integer a, Integer b) {
+      return a + b;
+    }
+  };
+
+  // but we don't use Java 8! so here is the small implementation of Streams and Functional Interfaces 
+  // that we have used above
+  public static class SmallJava8 {
+
+    /**
+     * Functional Interface similar to java.util.function.Function
+     */
+    public interface Function<I, O> {
+
+      /**
+       * Applies this function to the given argument
+       */
+      O apply(I input);
+    }
+
+    /**
+     * Functional Interface similar to java.util.function.BinaryOperator
+     */
+    public interface BinaryOperator<T> {
+
+      /**
+       * Applies this function to the given argument
+       */
+      T apply(T param1, T param2);
+    }
+
+    /**
+     * A sequence of elements supporting aggregate operations that works in a
+     * similar way as java.util.stream.Stream
+     */
+    public interface Stream<T> {
+
+      /**
+       * Returns a stream consisting of the results of applying the given
+       * function to the elements of this stream.
+       */
+      <R> Stream<R> map(Function<? super T, ? extends R> mapper);
+
+      /**
+       * Performs a reduction on the elements of this stream, using an
+       * associative accumulation function, and returns an Optional describing
+       * the reduced value, if any.
+       */
+      T reduce(T identity, BinaryOperator<T> accumulator);
+
+      /**
+       * Performs a reduction on the elements of this stream, using the provided
+       * identity value and an associative accumulation function, and returns
+       * the reduced value.
+       */
+      Optional<T> reduce(BinaryOperator<T> accumulator);
+
+    }
+
+    /**
+     * Implementation of SmallJava8.Stream backed by an array
+     */
+    public static class ArrayStream<T> implements Stream<T> {
+
+      private final T[] array;
+
+      private ArrayStream(T[] array) {
+        this.array = array;
+      }
+
+      @SuppressWarnings("unchecked")
+      @Override
+      public <R> Stream<R> map(Function<? super T, ? extends R> mapper) {
+
+        int length = array.length;
+        R[] outArray = (R[]) new Object[length];
+        for (int i = 0; i < length; i++) {
+          outArray[i] = mapper.apply(array[i]);
+        }
+        return new ArrayStream<R>(outArray);
+      }
+
+      @Override
+      public T reduce(T identity, BinaryOperator<T> accumulator) {
+        T result = identity;
+        for (T element : array) {
+          result = accumulator.apply(result, element);
+        }
+        return result;
+      }
+
+      @Override
+      public Optional<T> reduce(BinaryOperator<T> accumulator) {
+        boolean foundAny = false;
+        T result = null;
+        for (T element : array) {
+          if (!foundAny) {
+            foundAny = true;
+            result = element;
+          } else
+            result = accumulator.apply(result, element);
+        }
+        return foundAny ? (new Optional<T>(result)) : (new Optional<T>());
+      }
+    }
+
+    /**
+     * Utility to get a Stream from a java.util.List
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> Stream<T> streamFromList(List<T> list) {
+      T[] arr = (T[]) new Object[list.size()];
+      arr = list.toArray(arr);
+      Stream<T> ret = new ArrayStream<T>(arr);
+      return ret;
+    }
+
+    /**
+     * A container object which may or may not contain a non-null value.
+     */
+    public static class Optional<T> {
+
+      final T t;
+
+      public Optional(T t) {
+        this.t = t;
+      }
+
+      public Optional() {
+        this.t = null;
+      }
+
+      public T get() {
+        return t;
+      }
+    }
+  }
+
+}
